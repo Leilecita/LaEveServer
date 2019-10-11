ae9273bf3dcb018f543ca53614692a7caa8f4e79
leila <delcampo.leila@gmail.com>
2019-10-11 18:22:04 -0300

    cambios

--- controllers/ItemsOrderController.php
+++ controllers/ItemsOrderController.php
@@ -60,7 +60,7 @@ class ItemsOrderController extends BaseController
             $newItemOrder=array('order_id' => $item_order['order_id'], 'product_id' => $item_order['product_id'],
                 'product_descr' => $item_order['product_descr'] ,'quantity' => $differenceQuantity, 'price' => $item_order['price'],
                 'preci1' => $item_order['preci1'],'preci2' => $item_order['preci2'],'preci3' => $item_order['preci3'],'preci4' => $item_order['preci4'],
-                'preci5' => $item_order['preci5'],'loaded' => "false",'reasigned_quantity' => 'true','pendient_stock' => 'false');
+                'preci5' => $item_order['preci5'],'loaded' => "false",'reasigned_quantity' => 'true','pendient_stock' => 'false','billing' => $item_order['billing']);
 
             $res = $this->model->save($newItemOrder);
 
