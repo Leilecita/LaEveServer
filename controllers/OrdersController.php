cce9687e804aecf9e85724f01251e51fc5f13059
leila <delcampo.leila@gmail.com>
2019-10-19 15:49:03 -0300

    cambio

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -213,8 +213,8 @@ class OrdersController extends BaseController
 
         $newOrder =array('user_id'=>1,'client_id' => 2,'state' => "", 'state_check' => "check",'state_prepare' => "toprepare",'state_billing' => "tobilling",
             'state_delivery' => "todelivery",'tocheck' => "true",'toprepare' => "true",'tobilling' => "false",'todelivery' => "false",'observation' => "",
-            'total_amount' => 0.0, 'delivery_date'=> "",'loaded_by'=> "",'delivery_by' => "",'assigned_zone' => "", 'loaded_in' => "",
-            'signed' => "false", 'paid_out' => "false", 'paid_amount' => 0.0,'order_reasigned_id' => 11);
+            'total_amount' => 0.0, 'delivery_date'=> $next_date,'loaded_by'=> "",'delivery_by' => "",'assigned_zone' => "", 'loaded_in' => "",
+            'signed' => "false", 'paid_out' => "false", 'paid_amount' => 0.0);
 
         unset($newOrder['id']);
         $res=$this->model->save($newOrder);
