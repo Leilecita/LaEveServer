3a951036b72d3becbafe7fad546da5baa257ad9f
leila <delcampo.leila@gmail.com>
2019-10-23 12:54:17 -0300

    se arregla carga de productos2

--- admin/upload.php
+++ admin/upload.php
@@ -85,43 +85,6 @@ if (isset($_FILES["productos"]) && !$_FILES["productos"]["error"]){
     echo "error al procesar el archivo productos<br/>";
 }
 
-$countOk=0;
-$countError=0;
-if (isset($_FILES["productos"]) && !$_FILES["productos"]["error"]){
-    $productModel = new ProductModel();
-
-    $columnasProd = array("id","codart","desart","codbar","ubicac","oferta","cosart","descu1","descu2","descu3","descu4","cosfin","gasfle","gastos","cosrea","poriva","marge1","marge2","marge3","marge4","marge5","preci1","preci2","preci3","preci4","preci5","preci6","stocka","stomin","conten","desabr","moduni","modfin","codrub","codsub","codpro","codmar","fecalt","fecpre","marcas","concom","tipcos","consto","fecref","moddes","presen","mensaj","codbar2","codbar3","exeiva");
-
-    $table = new Table($_FILES["productos"]["tmp_name"]);
-
-    $columns = $table->getColumns();
-
-    while ($record = $table->nextRecord()) {
-
-        $s = [];
-        foreach ($columns as $column) {
-            if(in_array($column->name,$columnasProd)){
-                $s[$column->name] = iconv('CP1252', 'UTF-8', $record->forceGetString($column->name));
-                if ($s[$column->name] == null) {
-                    $s[$column->name] = '';
-                }
-            }
-        }
-        try {
-            $productModel->save($s);
-            //echo "Guardo " . $s['id'] . '<br/>';
-
-            $countOk++;
-        } catch (Exception $e) {
-            error_log('Error: ' . print_r($s, true));
-            $countError++;
-        }
-    }
-    echo " Se crearon $countOk productos y fallaron $countError <br/>";
-
-}else{
-    echo "error al procesar el archivo productos<br/>";
-}
 
 /*
 $countOk=0;
