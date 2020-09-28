0ecf82a8e82ad7f1c5dae60e086bee9d6a32abcc
leila <delcampo.leila@gmail.com>
2020-09-28 11:14:50 -0300

    cambios

--- models/OrderModel.php
+++ models/OrderModel.php
@@ -162,12 +162,4 @@ class OrderModel extends BaseModel
         $query = 'SELECT * FROM '.$this->tableName .( empty($filters) ?  '' : ' WHERE '.$conditions ).' ORDER BY created DESC';
         return $this->getDb()->fetch_all($query);
     }
-
-
-    function deleteEmptyOrders($filters){
-        $conditions = join(' AND ',$filters);
-        $query = 'DELETE FROM '.$this->tableName .( empty($filters) ?  '' : ' WHERE '.$conditions );
-        return $this->getDb()->fetch_row($query);
-    }
-
 }
