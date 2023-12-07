7924e14d39627b739d2bfdc885fccfb70d937453
leila <delcampo.leila@gmail.com>
2023-12-07 09:40:44 -0300

    optimizacion

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -53,7 +53,7 @@ class OrdersController extends SecureBaseController
     function asingFilters(){
         $filters=array();
 
-        $filters[] = 'o.created >= "'."2022-12-20 10:10:10".'"';
+       // $filters[] = 'o.created >= "'."2022-12-20 10:10:10".'"';
 
         if(isset($_GET['delivery_date'])) {
             if($_GET['delivery_date'] != 'todas') {
